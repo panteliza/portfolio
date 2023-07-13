79bf9ccf12f6c0ed213776e7f8111d8da90170e9
Eliza Pant <elizapant.github@gmail.com>
2023-07-13 23:05:19 +0545

    update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,53 +1,47 @@
-import React from 'react'
-import {BiHome } from 'react-icons/bi';
-import {GoPerson} from 'react-icons/go';
-import {HiDocument} from 'react-icons/hi';
-import {MdHomeRepairService} from 'react-icons/md';
-import {MdOutlinePhotoFilter} from 'react-icons/md';
-import {CiPaperplane} from 'react-icons/ci';
+import React from 'react';
+import { BiHome } from 'react-icons/bi';
+import { GoPerson } from 'react-icons/go';
+import { HiDocument } from 'react-icons/hi';
+import { MdHomeRepairService } from 'react-icons/md';
+import { MdOutlinePhotoFilter } from 'react-icons/md';
+import { CiPaperplane } from 'react-icons/ci';
 import { Link } from 'react-router-dom';
 
 export default function Navbar() {
   return (
-    <div>
-      <div className=" flex justify-between px-[100px] py-[30px] bg-[#212B36] fixed top-0 w-full">
-        <div className=" font-semibold text-white flex text-[30px] ">
+    <div className="navbar fixed top-0 w-full">
+      <div className="flex justify-between px-[100px] py-[30px] bg-[#212B36]">
+        <div className="font-semibold text-white flex text-[30px]">
           <div className='text-pink-500 '>E</div>
-       <div> liza</div>
+          <div>liza</div>
         </div>
-        <div className="flex justify-end gap-[40px] ">
-        <div className="flex flex-col items-center text-white
-        hover:text-pink-500">
-          <div className=""><BiHome/></div>
-          <div >Home</div>
+        <div className="flex justify-end gap-[40px]">
+          <div className="flex flex-col items-center text-white hover:text-pink-500">
+            <div className=""><BiHome /></div>
+            <div>Home</div>
           </div>
-          
-          <div className=" flex flex-col items-center  text-white
-          hover:text-pink-500">
-          <div className=""><GoPerson/></div>
-          <div>About</div>
+          <div className="flex flex-col items-center text-white hover:text-pink-500">
+            <div className=""><GoPerson /></div>
+            <div>About</div>
           </div>
-          <div className=" flex flex-col items-center  text-white
-          hover:text-pink-500">
-          <div className=""><HiDocument/></div>
-          <div>Skills</div>
+          <div className="flex flex-col items-center text-white hover:text-pink-500">
+            <div className=""><HiDocument /></div>
+            <div>Skills</div>
           </div>
-          <div className=" flex flex-col items-center  text-white
-          hover:text-pink-500">
-          <div className=""><MdHomeRepairService/></div>
-          <div>Services</div>
+          <div className="flex flex-col items-center text-white hover:text-pink-500">
+            <div className=""><MdHomeRepairService /></div>
+            <div>Services</div>
           </div>
-          <div className="flex flex-col items-center  text-white
-          hover:text-pink-500">
-          <div className=""><MdOutlinePhotoFilter/></div>
-          <div>Resume</div>
+          <div className="flex flex-col items-center text-white hover:text-pink-500">
+            <div className=""><MdOutlinePhotoFilter /></div>
+            <div>Resume</div>
           </div>
-          <div className=" flex flex-col items-center  text-white hover:text-pink-500">
-          <div className=""><CiPaperplane/></div>
-          <div>Contact</div>
+          <div className="flex flex-col items-center text-white hover:text-pink-500">
+            <div className=""><CiPaperplane /></div>
+            <div>Contact</div>
           </div>
-          </div>
+        </div>
       </div>
     </div>
-  )
+  );
 }
--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,7 +1,24 @@
-import React from 'react'
+import React,{useState} from 'react'
 import {FaGraduationCap  } from 'react-icons/fa';
 import {BiBriefcaseAlt2  } from 'react-icons/bi';
+import {FaReact  } from 'react-icons/fa';
+import {SiTailwindcss  } from 'react-icons/si';
+import {FiCode  } from 'react-icons/fi';
+
+import abc from '../assets/Asset 4.png'
 const Resume = () => {
+  const [isEducationClicked, setIsEducationClicked] = useState(true);
+  const [isExperienceClicked, setIsExperienceClicked] = useState(false);
+
+  const handleEducationClick = () => {
+    setIsEducationClicked(true);
+    setIsExperienceClicked(false)
+  };
+   const handleExperienceClick = () => {
+    setIsEducationClicked(false)
+      setIsExperienceClicked(true);
+    };
+
   return (
     <div className='flex flex-col items-center
     justify-center m-[50px]'>
@@ -9,22 +26,42 @@ const Resume = () => {
      <div className="text-gray-500">My personal journey</div>
 
      <div className='flex gap-10 mt-[60px]'>
-      <div className='flex gap-2  justify-center items-center '>
+     <div className={`flex gap-2  justify-center items-center cursor-default ${isEducationClicked ? 'text-pink-500' : 'inherit'} `}   onClick={handleEducationClick}>
         <FaGraduationCap className='text-[25px]'/>
-        <div className='text-[20px] font-semibold'>Education</div>
+        <div className='text-[20px] font-semibold' 
+      >Education</div>
       </div>
-      <div className='flex gap-2  justify-center items-center '>
+      <div className={`flex gap-2  justify-center items-center cursor-default ${isExperienceClicked ? 'text-pink-500' : 'inherit'} `}  onClick={handleExperienceClick}>
         <BiBriefcaseAlt2 className='text-[25px]'/>
-        <div className='text-[20px] font-semibold'>Experience</div>
+        <div className='text-[20px] font-semibold '
+       >Experience</div>
       </div>
+      
      </div>
      
       {/* For education */}
+     {/* <div className='flex gap-[20px]'>
+      <div className='relative'>
+        <img src={abc} className='w-[120px] h-[500px] mt-[40px]'/>
+        <div className='text-white text-[30px] absolute top-[88px] left-[45px]'><FaReact/></div>
+        <div className='text-white text-[30px] absolute top-[280px] left-[45px]'><SiTailwindcss/></div>
+        <div className='text-white text-[30px] absolute top-[470px] left-[45px]'><FiCode/></div>
+      </div>
+     
+     </div> */}
+
+
+     {/* for experience */}
+     {isEducationClicked && (
      <div className='flex gap-[20px]'>
-      <div></div>
-      <div></div>
-      <div></div>
+      <div className='relative'>
+        <img src={abc} className='w-[120px] h-[500px] mt-[40px]'/>
+        <div className='text-white text-[30px] absolute top-[88px] left-[45px]'><FaReact/></div>
+        <div className='text-white text-[30px] absolute top-[280px] left-[45px]'><SiTailwindcss/></div>
+        <div className='text-white text-[30px] absolute top-[470px] left-[45px]'><FiCode/></div>
+      </div>
      </div>
+   ) }
 
     </div>
   )
