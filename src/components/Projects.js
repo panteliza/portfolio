9ac10c9b00bf42636b18d2abff1e5dbceb8a0cbd
Eliza Pant <elizapant.github@gmail.com>
2023-07-15 17:56:51 +0545

    project completed

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -7,22 +7,21 @@ export default function Projects() {
     <div className="text-gray-500">Highlights of my Work</div>
       <div className='flex  '>
       
-        <div className='grid grid-cols-3 justify-center items-center gap-12 pt-[20px]  '>
+        <div className='grid grid-cols-3 justify-evenly gap-5 items-center  pt-[20px]  '>
         <div className='w-[420px] h-[280px] rounded-xl relative group'>
-  <img src="https://images.unsplash.com/photo-1550828486-68812fa3f966?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80" className='w-[420px] h-[280px] rounded-xl border border-gray-900 relative' />
+  <img src="https://images.unsplash.com/photo-1550828486-68812fa3f966?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80" className='w-[420px] h-[280px] rounded-xl border border-pink-500 relative' />
   <div className='absolute top-0 left-0 w-full h-full bg-gradient-to-r from-violet-500 to-fuchsia-500 opacity-50 rounded-xl group-hover:opacity-0 transition-opacity duration-300'></div>
   <div className=' '>
-    <div className=' rounded-3xl bg-green-500 text-black font-semibold top-5 absolute right-2 px-3 py-2 hover:bg-black hover:text-green-500 text-[14px]'>Online Gaming</div>
-    <div className='text-white font-semibold bottom-28 absolute left-5 opacity-[2px] '>May 20, 2023</div>
+    <div className=' rounded-3xl bg-green-500 text-black font-semibold top-5 cursor-default absolute right-2 px-3 py-2 hover:bg-black hover:text-green-500 text-[14px]'>Online Gaming</div>
     <div className='text-white font-semibold bottom-10 absolute left-5 text-2xl pr-8'>Influence of Online Game Behaviors on Emotional States</div>
   </div>
 </div>
 
         <div className='w-[420px] h-[280px] rounded-xl relative group'>
-        <img src="https://media.istockphoto.com/id/1145166058/photo/retro-old-microphone-vintage-style-with-neon-lights.webp?b=1&s=170667a&w=0&k=20&c=9dfmza0BqYIHatlP5lhoMP1v8X6kvFdqLBDeactXikM=" className='w-[420px] h-[280px]  rounded-xl border border-gray-900'/>
+        <img src="https://media.istockphoto.com/id/1145166058/photo/retro-old-microphone-vintage-style-with-neon-lights.webp?b=1&s=170667a&w=0&k=20&c=9dfmza0BqYIHatlP5lhoMP1v8X6kvFdqLBDeactXikM=" className='w-[420px] h-[280px]  rounded-xl border border-pink-500'/>
         <div className='absolute top-0 left-0 w-full h-full bg-gradient-to-r from-violet-500 to-fuchsia-500 opacity-50 rounded-xl group-hover:opacity-0 transition-opacity duration-300'></div>
         <div className=' '>
-    <div className=' rounded-3xl bg-green-500 text-black font-semibold  top-5 absolute right-2 px-3 py-2 hover:bg-black hover:text-green-500 text-[14px]'>Entertainment</div>
+    <div className=' rounded-3xl bg-green-500 text-black font-semibold cursor-default top-5 absolute right-2 px-3 py-2 hover:bg-black hover:text-green-500 text-[14px]'>Entertainment</div>
     <div className='text-white font-semibold bottom-28 absolute left-5 '> May 20, 2023</div>
     <div className='text-white font-semibold bottom-10 absolute left-5 text-2xl pr-8'> Music changes the world, music for everybody </div>
   </div>
@@ -30,35 +29,34 @@ export default function Projects() {
 
 
         <div className='w-[420px] h-[280px] rounded-xl  relative group'>
-        <img src="https://media.istockphoto.com/id/1486013436/photo/summer-holiday-travel-objects-on-neon-lighting-background.webp?b=1&s=170667a&w=0&k=20&c=4ORbBRMsZNB4wcdDg6w5qo2DM9Xyt4xEDjp8Aue6kg8=" className='w-[420px] h-[280px]  rounded-xl border border-gray-900'/>
+        <img src="https://media.istockphoto.com/id/1486013436/photo/summer-holiday-travel-objects-on-neon-lighting-background.webp?b=1&s=170667a&w=0&k=20&c=4ORbBRMsZNB4wcdDg6w5qo2DM9Xyt4xEDjp8Aue6kg8=" className='w-[420px] h-[280px]  rounded-xl border border-pink-500'/>
         <div className='absolute top-0 left-0 w-full h-full bg-gradient-to-r from-violet-500 to-fuchsia-500 opacity-50 rounded-xl group-hover:opacity-0 transition-opacity duration-300'></div>
         <div className=' '>
-    <div className=' rounded-3xl bg-green-500 text-black font-semibold  top-5 absolute right-2 px-3 py-2 hover:bg-black hover:text-green-500 text-[14px]'>Explore</div>
-    <div className='text-white font-semibold bottom-28 absolute left-5'> May 20, 2023</div>
+    <div className=' rounded-3xl bg-green-500 text-black font-semibold cursor-default top-5 absolute right-2 px-3 py-2 hover:bg-black hover:text-green-500 text-[14px]'>Explore</div>
     <div className='text-white font-semibold bottom-10 absolute left-5 text-2xl pr-8'> Explore the world with the best guidance ever </div>
   </div>
          </div>
 
 
         <div className='w-[420px] h-[280px] rounded-xl relative group'>
-        <img src="https://media.istockphoto.com/id/1388988332/photo/cyborg-running-on-purple-background-3d-wired-shape.webp?b=1&s=170667a&w=0&k=20&c=ZsKLspVyfbxfSGHTEHMiqQWRXGAKxuhqM6fLJnnb5ok=" className='w-[420px] h-[280px]  rounded-xl border border-gray-900'/>
+        <img src="https://media.istockphoto.com/id/1388988332/photo/cyborg-running-on-purple-background-3d-wired-shape.webp?b=1&s=170667a&w=0&k=20&c=ZsKLspVyfbxfSGHTEHMiqQWRXGAKxuhqM6fLJnnb5ok=" className='w-[420px] h-[280px]  rounded-xl border border-pink-500'/>
         <div className='absolute top-0 left-0 w-full h-full bg-gradient-to-r from-violet-500 to-fuchsia-500 opacity-50 rounded-xl group-hover:opacity-0 transition-opacity duration-300'></div>
         <div className=' '>
-    <div className=' rounded-3xl bg-green-500 text-black font-semibold  top-5 absolute right-2 px-3 py-2 hover:bg-black hover:text-green-500 text-[14px]'>Artificial Intelligence</div>
-    <div className='text-white font-semibold bottom-28 absolute left-5'> May 20, 2023</div>
+    <div className=' rounded-3xl bg-green-500 text-black font-semibold cursor-default top-5 absolute right-2 px-3 py-2 hover:bg-black hover:text-green-500 text-[14px]'>Artificial Intelligence</div>
+    
     <div className='text-white font-semibold bottom-10 absolute left-5 text-2xl pr-8'> AI powered Personal Assistant for Windows </div>
   </div>
         </div>
 
 
         <div className='w-[420px] h-[280px] rounded-xl relative group'>
-  <img src="https://media.istockphoto.com/id/1284053136/photo/young-sportive-woman-training-isolated-on-gradient-studio-background-in-neon-light-athletic.webp?b=1&s=170667a&w=0&k=20&c=JUJKEBWcf-PhGHL5Ap-aSVwafLUnxOrlH6Zod_pHSMo=" className='w-[420px] h-[280px] rounded-xl border border-gray-900'/>
+  <img src="https://media.istockphoto.com/id/1284053136/photo/young-sportive-woman-training-isolated-on-gradient-studio-background-in-neon-light-athletic.webp?b=1&s=170667a&w=0&k=20&c=JUJKEBWcf-PhGHL5Ap-aSVwafLUnxOrlH6Zod_pHSMo=" className='w-[420px] h-[280px] rounded-xl border border-pink-500'/>
   <div className='absolute top-0 left-0 w-full h-full bg-gradient-to-r from-violet-500 to-fuchsia-500 opacity-50 rounded-xl group-hover:opacity-0 transition-opacity duration-300'></div>
   <div className=''>
-    <div className='rounded-3xl bg-green-500 text-black font-semibold top-5 right-2 px-3 py-2 hover:bg-black hover:text-green-500 text-[14px] absolute'>
+    <div className='rounded-3xl bg-green-500 text-black font-semibold cursor-default top-5 right-2 px-3 py-2 hover:bg-black hover:text-green-500 text-[14px] absolute'>
       Fitness
     </div>
-    <div className='text-white font-semibold bottom-28 absolute left-5'>May 20, 2023</div>
+    
     <div className='text-white font-semibold bottom-10 absolute left-5 text-2xl pr-8'>
       Prepare your fitness now, Life Hacks to rule
     </div>
@@ -70,13 +68,13 @@ export default function Projects() {
 
         <div className="w-[420px] h-[280px] rounded-xl relative group">
   <img src="https://images.unsplash.com/photo-1475483768296-6163e08872a1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80"
-    className="w-[420px] h-[280px] rounded-xl border border-gray-900"
+    className="w-[420px] h-[280px] rounded-xl border border-pink-500"
     alt="Image"
   />
    <div className='absolute top-0 left-0 w-full h-full bg-gradient-to-r from-violet-500 to-fuchsia-500 opacity-40 rounded-xl group-hover:opacity-0 transition-opacity duration-300'></div>
   <div>
-    <div className="rounded-3xl bg-green-500 text-black font-semibold top-5 absolute right-2 px-3 py-2 hover:bg-black hover:text-green-500 text-[14px]">Travelling</div>
-    <div className="text-white font-semibold bottom-28 absolute left-5 ">April 29, 2023</div>
+    <div className="rounded-3xl bg-green-500 text-black font-semibold cursor-default top-5 absolute right-2 px-3 py-2 hover:bg-black hover:text-green-500 text-[14px]">Travelling</div>
+    
     <div className="text-white font-semibold bottom-10 absolute left-5 text-2xl pr-8">National Parks in California, A Guide for Nature Lovers</div>
   </div>
 </div>
