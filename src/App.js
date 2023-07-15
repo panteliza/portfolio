6242bb4b3796fe3695b049f11a3ce58385adf54f
Eliza Pant <elizapant.github@gmail.com>
2023-07-15 21:55:41 +0545

    update

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 import Home from './components/Home';
 import About from './components/About';
-import Skills from './components/Skills';
+
 import Projects from './components/Projects';
 import Resume from './components/Resume';
 import Contact from './components/Contact';
